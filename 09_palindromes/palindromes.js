0db5eb70c129e7b9e5b975cd35a4bf091c2ef540
Frankie Xiao <fxiaoX089@live.com>
2022-06-06 22:23:43 -0500

    Palindrome works

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const { truncate } = require("fs/promises");
 
+/**
+ * Splices string and checks for palindrome using for loop
+ * @param {string} string to be spliced
+ * @returns true or false
+ */
+const palindromesForLoop = function (string) {
+    // Need to splice out white spaces and puncutation.  Covert all letters to same casing. 
+    const str = string.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+
+    for(let i = 0; i < str.length/2; i++){
+        if(str[i] != str[str.length - 1 - i])
+            return false;
+    }
+    return true;
 };
+
+/**
+ * Splices string and checks for palindrome using built in functions
+ * @param {string} string string to be spliced
+ * @returns true or false
+ */
+const palindromes = function(string){
+    const regex = /[\W_]/g;
+    const regStr = string.toLowerCase().replace(regex, '');
+    const reverseStr = regStr.split('').reverse().join('');
+    return regStr === reverseStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
