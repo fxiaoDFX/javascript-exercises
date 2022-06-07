885f27ee12a0f15c32de8b7d90314305010119a3
Frankie Xiao <fxiaoX089@live.com>
2022-06-06 21:28:34 -0500

    Calculator Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((x , y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((x, y) => x * y);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  if(y < 0)
+    return -1;
+	return y === 0 ? 1 : x * power(x, y - 1); 
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	return x <= 1 ? 1 : x * factorial(x - 1);
 };
 
 // Do not edit below this line
