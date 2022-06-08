d9b6257c570dccd4785d9728ef060a946801e2ff
Frankie Xiao <fxiaoX089@live.com>
2022-06-07 22:09:04 -0500

    Fibonacci Function Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+// Recursive formula for fibonacci sequence is a_1 = 1, a_2 = 1 and a_n = a_n-2 + a_n-1 for n >= 3.  Can use recursion to calculate fibonacci sequence.   
 
+const fibonacci = function(x) {
+    // covert strings of numbers to Number type
+    const number = Number(x);
+    if(number < 0){
+        return "OOPS";
+    }
+    if(number <= 1)
+        return number;
+    return fibonacci(number - 2) + fibonacci(number - 1);
 };
+
+//console.log(fibonacci("11"))
 
 // Do not edit below this line
 module.exports = fibonacci;
