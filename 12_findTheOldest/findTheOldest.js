f706aa6ecda77d7908e64b2e2b1592fc9b0a31f7
Frankie Xiao <fxiaoX089@live.com>
2022-06-08 10:31:53 -0500

    Function Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+/**
+ * Finds the oldest person, this function doesn't return multiple people when multiple are the same age
+ * @param {object} people is an array of objects
+ * @returns the oldest object
+ */
+const findTheOldest = function(people) {
+    const oldest = people.reduce((p1, p2) => {
+        //  Check for year of death, use current year if year of death is not found
+        if(p1.yearOfDeath === undefined)
+            p1.yearOfDeath = (new Date()).getFullYear();
+        // Calculate ages and return the older one
+        age1 = p1.yearOfDeath - p1.yearOfBirth;
+        age2 = p2.yearOfDeath - p2.yearOfBirth;
+        return age1 > age2 ? p1 : p2;
+    })
+    return oldest;
 };
 
 // Do not edit below this line
