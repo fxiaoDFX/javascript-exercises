88194bdf84cac925c777e7134220a80c09e6acc0
Frankie Xiao <fxiaoX089@live.com>
2022-06-08 10:45:04 -0500

    Update formatting

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -4,16 +4,15 @@
  * @returns the oldest object
  */
 const findTheOldest = function(people) {
-    const oldest = people.reduce((p1, p2) => {
+    return oldest = people.reduce((p1, p2) => {
         //  Check for year of death, use current year if year of death is not found
-        if(p1.yearOfDeath === undefined)
+        if(!p1.yearOfDeath)
             p1.yearOfDeath = (new Date()).getFullYear();
         // Calculate ages and return the older one
         age1 = p1.yearOfDeath - p1.yearOfBirth;
         age2 = p2.yearOfDeath - p2.yearOfBirth;
         return age1 > age2 ? p1 : p2;
     })
-    return oldest;
 };
 
 // Do not edit below this line
